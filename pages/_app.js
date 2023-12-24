e4377bf7849f5dc1e02f15b93c6cb670bffce53b
Kevin Knights <kevinknights29@hotmail.com>
2023-12-24 15:51:38 -0500

    fix: export added

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../src/app/globals.css'; // Adjust the path to your global styles if necessary
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
